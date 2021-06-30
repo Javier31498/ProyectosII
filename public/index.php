fa7efd0ddefdc81bc619e218154a9ed62923cbbc
Javier31498 <javi31498@gmail.com>
2021-06-30 20:09:53 +0200

    Cambio 3

--- public/index.php
+++ public/index.php
@@ -48,8 +48,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
-$response = $kernel->handle(
+$response = tap($kernel->handle(
     $request = App\Custom\Http\Request::capture()
-);
+))->send();
 
 $kernel->terminate($request, $response);
